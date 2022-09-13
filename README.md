2701daa0cc051095dab45b15c0443db6b6186e6b
yousefaldabbas0@gmail.com <yousefaldabbas0@gmail.com>
2022-09-13 10:57:13 +0300

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,38 @@
-# Getting Started with Create React App
+# Quotes Generator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![Design preview](./screenshots/1.png)
 
-## Available Scripts
+## Welcome! 👋
 
-In the project directory, you can run:
+Thanks for checking out this project!
 
-### `npm start`
+The goal of this project is to develop my understanding of typescript and to improve my web layout skills by building a realistic project .
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Table of contents
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
 
-### `npm test`
+## Overview
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Screenshot
 
-### `npm run build`
+![Screenshot](./screenshots/1.png)
+![Screenshot](./screenshots/2.png)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## My process
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Built with
+#### frontend technology
+- React
+- typescript
+- tailwindcss
+- axios
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Author
+- Twitter - [@joseph2s5](https://www.twitter.com/joseph2s5)
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header"
+
+
+type headers = {
+  "X-Api-Key": string;
+  "Content-Type": string;
+};
+
+type getQuotes = {
+  quotes: quotes;
+};
+type quotes = quote[];
+
+type quote = {
+  quote: string;
+  author: string;
+  category: string;
+};
+const headers = {
+  "X-Api-Key": process.env.REACT_APP_API_KEY||"",
+  "Content-Type": "application/json",
+};
 
 function App() {
+  const [quotes, setQuotes] = useState<quotes>([
+    {
+      quote: "The will of man is his happiness.",
+      author: "Friedrich Schiller",
+      category: "happiness",
+    },
+  ]);
+  const [currentQuote, setCurrentQuote] = useState<number>(0);
+  useEffect(() => {
+    axios
+      .get<getQuotes, any>(
+        "https://api.api-ninjas.com/v1/quotes?category=happiness&limit=20",
+        {
+          headers,
+        }
+      )
+      .then(function (response) {
+        setQuotes((prev) => [...prev, ...response.data]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  function getAnotherQuote() {
+    setCurrentQuote(Math.floor(Math.random() * quotes.length));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="inline-block w-8 h-8 text-gray-400 mb-8"
+              viewBox="0 0 975.036 975.036"
+            >
+              <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+            </svg>
+            <p
+              className="leading-relaxed text-lg"
+              dangerouslySetInnerHTML={{ __html: quotes[currentQuote].quote }}
+            ></p>
+            <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+            <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+              {quotes[currentQuote].author.toLowerCase()}
+            </h2>
+            <button className="btn" onClick={getAnotherQuote}>
+              another one
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
